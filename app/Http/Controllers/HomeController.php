0faffb7558b94402f988ea5a09f55328cdf4e46a
gaurav <gauravp@zignuts.com>
2023-03-01 12:02:58 +0530

    Uncomment the welcome email code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -57,7 +57,7 @@ class HomeController extends Controller
         ]);
 
         //Welcome Mail
-        //Mail::to($user->email)->send(new WelcomeMail($user));
+        Mail::to($user->email)->send(new WelcomeMail($user));
 
         //Response
         return response()->json([
